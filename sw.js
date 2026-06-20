1430589265cc0421243b0741504b5b0b5e93e2e9
jimmywiwi11-wq <jimmywiwi11@gmail.com>
2026-06-20 14:45:01 +0700

    v7.0

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='stock-alert-v6-8-update-system';
+const CACHE_NAME='stock-alert-v7-0-update-name';
 const ASSETS=['./','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png','./version.json'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)).catch(()=>{}));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim();});
